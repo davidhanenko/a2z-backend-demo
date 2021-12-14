ea269c43959aea765f11cddf67efed71a8c017b9
davidhanenko <d.hanenko24@gmail.com>
2021-12-14 06:00:46 -0500

    env

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,12 +1,16 @@
 module.exports = {
   settings: {
+    load: {
+      before: ['responseTime', 'logger', 'cors', 'responses'],
+      after: ['parser', 'router'],
+    },
     cors: {
       origin: [
         'https://a2z-frontend-demo.vercel.app',
         'https://a2z-demo.herokuapp.com',
         'http://localhost:1337',
         'http://localhost:7777',
-      ]
+      ],
     },
   },
 };

c336c7a71d39da246ccabe1f4fe1426a302ddd0c
davidhanenko <d.hanenko24@gmail.com>
2021-12-13 23:44:46 -0500

    env

--- config/middleware.js
+++ config/middleware.js
@@ -2,15 +2,14 @@ module.exports = {
   //...
   settings: {
     cors: {
-      origin: ['http://localhost:1337', 'http://localhost:7777', 
-    ],
+      origin: ['http://localhost:1337', 'http://localhost:7777'],
     },
     // 10 Mb max upload file size
     parser: {
       enabled: true,
       multipart: true,
       formidable: {
-        maxFileSize: 10 * 1024 * 1024
+        maxFileSize: 10 * 1024 * 1024,
       },
     },
   },

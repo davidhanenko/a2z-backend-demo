9e15834b076836aaee3d93d3144a13aa0573db3d
davidhanenko <d.hanenko24@gmail.com>
2021-12-15 00:04:37 -0500

    predeploy

--- config/middleware.js
+++ config/middleware.js
@@ -3,11 +3,12 @@ module.exports = {
   settings: {
     cors: {
       origin: [
-        '*'
-        // 'https://a2z-frontend-demo.vercel.app',
-        // 'https://a2z-demo.herokuapp.com',
-        // 'http://localhost:1337',
-        // 'http://localhost:7777',
+        'https://a2z-frontend-demo.vercel.app',
+        'https://a2z-demo.herokuapp.com',
+        'https://a2z-frontend-demo.vercel.app/',
+        'https://a2z-demo.herokuapp.com/',
+        'http://localhost:1337',
+        'http://localhost:7777',
       ],
     },
     // 10 Mb max upload file size

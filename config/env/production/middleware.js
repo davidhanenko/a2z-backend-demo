8471a0a7543b45a90dd204d4a289d82106cd8d38
davidhanenko <d.hanenko24@gmail.com>
2021-12-14 23:29:37 -0500

    predeploy

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -2,7 +2,6 @@ module.exports = {
   settings: {
     cors: {
       origin: [
-        '*',
         'https://a2z-frontend-demo.vercel.app/',
         'https://a2z-demo.herokuapp.com/',
         'http://localhost:1337',

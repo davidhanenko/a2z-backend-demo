8abd35c51fca85951b64687d7b2539436aa543af
davidhanenko <d.hanenko24@gmail.com>
2021-12-15 19:55:10 -0500

    lock

--- config/middleware.js
+++ config/middleware.js
@@ -5,8 +5,6 @@ module.exports = {
       origin: [
         'https://a2z-frontend-demo.vercel.app',
         'https://a2z-demo.herokuapp.com',
-        'https://a2z-frontend-demo.vercel.app/',
-        'https://a2z-demo.herokuapp.com/',
         'http://localhost:1337',
         'http://localhost:7777',
       ],
